482edb7fe64fececdb864aac3b107da3a6d2b441
AwaisShaikh07 <shaikhworkers@gmail.com>
2023-12-05 14:13:12 +0500

    Test

---
+++ Test1.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import Page,Playwright, expect
+
+
+def test_example(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.saucedemo.com/")
+    page.locator("[data-test=\"username\"]").click()
+    page.locator("[data-test=\"username\"]").fill("standard_user")
+    page.locator("[data-test=\"password\"]").click()
+    page.locator("[data-test=\"password\"]").fill("secret_sauce")
+    page.locator("[data-test=\"login-button\"]").click()
+    page.get_by_role("button", name="Open Menu").click()
+    page.get_by_role("link", name="Logout").click()
+    expect(page.locator("#header_container")).to_contain_text("Products")
+
+    # ---------------------
+    context.close()
+    browser.close()
